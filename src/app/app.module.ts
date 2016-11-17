6146ec0cf9da524623884f16956007931723f7a4
megalucio <megalucio@gmail.com>
2016-11-17 09:07:50 -0500

    With email service not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,10 @@ import {About} from './components/about/about';
 import {Home} from './components/home/home';
 import {Friends} from './components/friends/friends';
 import {ProductComponent} from './components/product/product';
-import {Cart} from './components/cart/cart';
 import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, About, Home, Friends, Cart, IterablePipe, ProductComponent],
+  declarations: [AppComponent, About, Home, Friends, IterablePipe, ProductComponent],
   imports     : [BrowserModule, FormsModule, HttpModule, JsonpModule, RouterModule.forRoot(rootRouterConfig)],
   providers   : [Constants, {provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap   : [AppComponent]
--- src/app/components/friends/friends.ts
+++ src/app/components/friends/friends.ts
@@ -1,19 +1,27 @@
-import { Cart } from './../cart/cart';
 import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from './../../services/product.service';
+import { EmailService } from './../../services/email.service';
 
 @Component({
   selector: 'friends',
   styleUrls: ['./friends.css'],
-  templateUrl: './friends.html'
+  templateUrl: './friends.html',
+  providers: [EmailService]
 })
 export class Friends {
+
+  constructor(private emailService: EmailService) {}
 
   emails: String[] = [];
 
   add(email: string) {
     this.emails.push(email);
+  }
+
+  sendAll(){
+    for(let email in this.emails){
+        this.emailService.sendEmail(email, "te toca enviarle el este a falete");
+    }
   }
 
                    
